aac6c3632c73d1e064f4d27ef711e16a0db77900
ryanvann33 <rvann377@gmail.com>
2022-11-10 13:58:07 -0500

    pepepeppe

--- main.js
+++ main.js
@@ -3,29 +3,17 @@
 // DOMSelectors are objects that hold access to the DOM that you can call upon easily at any point in your code.
 
 const DOMSelectors = {
-  button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
-  text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
   box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
-  null: document.getElementById("point"), // if you try to access a class with getElementById, it will result in "null"
-  empty: document.querySelector(".nothing"),
 };
+console.log(DOMSelectors.box);
 function backgroundAndText(background, text) {
   background.style.backgroundColor = "red";
   text.innerHTML = "This is now a big red box";
-  text.style.fontSize = "40px";
+  text.style.fontSize = "30px";
 }
 
 DOMSelectors.button.addEventListener("click", function () {
   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
 });
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-
-function changeLi() {
-  DOMSelectors.points.forEach((point) => {
-    point.addEventListener("click", function () {
-      point.textContent = "Hello";
-    });
-  });
-}
-changeLi();
